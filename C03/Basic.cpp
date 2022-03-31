f8421a424b2bad7a4133e826b4a547fc6a382f9e
Nanoha <1371689491@qq.com>
2022-03-31 21:25:08 +0800

    no message

---
+++ C03/Basic.cpp
@@ -0,0 +1,17 @@
+//: C03:Basic.cpp
+// Defining the four basic data
+// types in C and C++
+
+int main() {
+    // Definition without initialization:
+    char protein;
+    int carbohydrates;
+    float fiber;
+    double fat;
+    // Simultaneous definition & initialization
+    char pizza = 'A', pop = 'z';
+    int dongdings = 100, twinkles = 150, heehos = 200;
+    float chocolate = 3.14159;
+    // Exponential notation
+    double fudge_ripple = 6e-4;
+}
